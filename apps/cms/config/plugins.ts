0803c2029bfdebe19b6cf8fd963f697864cf7019
Tim Hrdy <tim.hrdy.1@gmail.com>
2026-02-20 11:18:13 +0100

    Refactor AWS S3 plugin configuration for improved structure
    - Updated the AWS S3 provider options to use a nested `credentials` object for access keys.
    - Changed the region configuration to allow for dynamic setting via environment variables.

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -4,10 +4,12 @@ export default ({ env }) => ({
       provider: 'aws-s3',
       providerOptions: {
         s3Options: {
-          accessKeyId: env('R2_ACCESS_KEY_ID'),
-          secretAccessKey: env('R2_ACCESS_SECRET'),
-          endpoint: env('R2_ENDPOINT'), // Format: https://<account_id>.r2.cloudflarestorage.com
-          region: 'auto',
+          credentials: {
+            accessKeyId: env('R2_ACCESS_KEY_ID'),
+            secretAccessKey: env('R2_ACCESS_SECRET'),
+          },
+          endpoint: env('R2_ENDPOINT'),
+          region: env('R2_REGION', 'auto'),
           params: {
             Bucket: env('R2_BUCKET'),
           },
