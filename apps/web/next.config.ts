6b36d42f2e95a480dc6bdfa97a29dbc439c7a02d
Tim Hrdy <tim.hrdy.1@gmail.com>
2026-03-12 12:36:24 +0100

    Add new security configuration for Strapi media subdomain in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.media.strapiapp.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
